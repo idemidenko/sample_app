713cfed54a7e3b22f7eda89c3bea86ddeaa9cbb1
Ilya Demidenko <idzemidzenka@iba.by>
2015-07-29 17:03:15 +0300

    revert project name changes and add controllers

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SampleApp.application.config.action_dispatch.cookies_serializer = :json
+Rails.application.config.action_dispatch.cookies_serializer = :json
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SampleApp.application.config.session_store :cookie_store, key: '_sample_app_session'
+Rails.application.config.session_store :cookie_store, key: '_sample_app_session'
